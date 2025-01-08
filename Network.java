3823a30d4d969df82fc9c93344c24ee295b8e002
maayanlevi2001 <maayanlevi2001@gmail.com>
2025-01-08 10:06:16 -0800

    exercise 1

--- Network.java
+++ Network.java
@@ -1,3 +1,5 @@
+
+
 /** Represents a social network. The network has users, who follow other uesrs.
  *  Each user is an instance of the User class. */
 public class Network {
@@ -29,41 +31,77 @@ public class Network {
      *  If there is no such user, returns null.
      *  Notice that the method receives a String, and returns a User object. */
     public User getUser(String name) {
-        //// Replace the following statement with your code
+        for (int i = 0; i < userCount; i ++){
+          if (name.equals(users[i].getName())){
+            return users[i];
+          } 
+        }
         return null;
-    }
+     }
+
 
     /** Adds a new user with the given name to this network.
     *  If ths network is full, does nothing and returns false;
     *  If the given name is already a user in this network, does nothing and returns false;
     *  Otherwise, creates a new user with the given name, adds the user to this network, and returns true. */
     public boolean addUser(String name) {
-        //// Replace the following statement with your code
-        return false;
+        if (this.userCount == this.users.length) return false;
+        if (this.getUser(name) != null)return false;
+        users[userCount] = new User(name);
+        userCount++;
+        return true;
     }
+     
+
 
     /** Makes the user with name1 follow the user with name2. If successful, returns true.
      *  If any of the two names is not a user in this network,
      *  or if the "follows" addition failed for some reason, returns false. */
     public boolean addFollowee(String name1, String name2) {
-        //// Replace the following statement with your code
-        return false;
-    }
+       if (name1 == null || name2 == null) return false;
+       if (getUser(name1) == null || getUser(name2) == null) return false;
+       if (name1.equals(name2)) return true;
+
+    return this.getUser(name1).addFollowee(name2);
+}
+
     
     /** For the user with the given name, recommends another user to follow. The recommended user is
      *  the user that has the maximal mutual number of followees as the user with the given name. */
     public String recommendWhoToFollow(String name) {
-        //// Replace the following statement with your code
-        return null;
+        User mostRecommendedUserToFollow = null;
+        User user = getUser(name);
+        if (name == null) return null;
+        if (user == null) return null;
+       
+        int maxMatualNum = 0;
+    
+                for(int i=0; i< this.userCount; i++){
+                    if(user.getName().equals(this.users[i].getName() ))
+                        continue;
+                    if (users[i].countMutual(getUser(name)) > maxMatualNum) {
+                    mostRecommendedUserToFollow = users[i];
+                    maxMatualNum = users[i].countMutual(getUser(name));
+                }
+            }
+        return mostRecommendedUserToFollow.getName();
     }
+       
 
     /** Computes and returns the name of the most popular user in this network: 
      *  The user who appears the most in the follow lists of all the users. */
     public String mostPopularUser() {
-        //// Replace the following statement with your code
-        return null;
+        String mostPopular = null;
+        int countPop = 0;
+        for(int i=0; i<this.userCount; i++){
+            if(this.followeeCount(this.users[i].getName()) > countPop){
+                countPop = this.followeeCount(this.users[i].getName());
+                mostPopular = this.users[i].getName();
+            }
+        }
+        return mostPopular;
     }
-
+       
     /** Returns the number of times that the given name appears in the follows lists of all
      *  the users in this network. Note: A name can appear 0 or 1 times in each list. */
     private int followeeCount(String name) {
@@ -76,4 +114,4 @@ public class Network {
        //// Replace the following statement with your code
        return null;
     }
-}
+    }
