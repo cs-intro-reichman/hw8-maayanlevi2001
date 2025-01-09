db1e9270ed24e073063f360f92a389be9a37e56c
maayanlevi2001 <maayanlevi2001@gmail.com>
2025-01-09 00:14:54 -0800

    exercise 1

--- User.java
+++ User.java
@@ -72,23 +72,19 @@
     /** Removes the given name from the follows list of this user. If successful, returns true. indexname = 2
      *  If the name is not in the list, does nothing and returns false. [roy,shira,yefet,mika,adi,noa,null,null] */
     public boolean removeFollowee(String name) {
-        int indexName = 0;
-        if(!this.follows(name)) { 
-            return false;
-        }
-        for ( int i=0; i<fCount - 1; i++) {
-            if (this.follows[i] != null && this.follows[i].equals(name)) {
-                indexName = i;
+         for (int i = 0; i < fCount; i++) {
+             if (this.follows[i].equals(name)) {
+                for (int j = i; j < fCount - 1; j++) {
+                this.follows[j] = this.follows[j + 1];
+                }
+               this.follows[fCount - 1] = null;
+               fCount--;
+               return true;
             }
-        }   
-        for (int i =indexName ; i < fCount-1; i ++){
-            this.follows[i] = this.follows[i-1];
         }
-        this.follows[fCount - 1] = null;
-        fCount--;
-        return true;
-    }
-
+         return false;
+        }
+   
     /** Counts the number of users that both this user and the other user follow.
     /*  Notice: This is the size of the intersection of the two follows lists. */
     public int countMutual(User other) {
